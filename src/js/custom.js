3166361418fa7181076937b333d772f352932ae1
tazbinahad <tazbinahad20@gmail.com>
2022-01-03 14:43:38 +0600

    ResponsiveHomePage

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,29 +1,31 @@
-document.addEventListener('DOMContentLoaded', function (event) {
-  console.log('DOM Is Ready for manipulation');
-  // -----------------------------------------
-  // SideBar
+// document.addEventListener('DOMContentLoaded', function (event) {
+//   console.log('DOM Is Ready for manipulation');
+// });
+
+// -----------------------------------------
+// Click event functions
+// SideBar toggle
+function Toggle() {
   const sideBarMenu = document.getElementById('sideBar');
   const toggleBtn = document.getElementById('toggleBtn');
   const Main = document.getElementById('main');
   const Footer = document.getElementById('footer');
-  const floatingCart = document.getElementById('floatingCart');
-
-  toggleBtn.addEventListener('click', function () {
-    sideBarMenu.classList.toggle('sideBarToggle');
-    Main.classList.toggle('sideBarToggle');
-    Footer.classList.toggle('sideBarToggle');
-  });
-});
-// -----------------------------------------
-// Click event functions
+  sideBarMenu.classList.toggle('sideBarToggle');
+  Main.classList.toggle('sideBarToggle');
+  Footer.classList.toggle('sideBarToggle');
+}
+// Open Floating Cart
 function openCart() {
+  const floatingCart = document.getElementById('floatingCart');
   let hasClass = floatingCart.classList.contains('closed');
   if (hasClass) {
     floatingCart.classList.remove('closed');
     floatingCart.classList.add('open');
   }
 }
+// Close Floating Cart
 function closeCart() {
+  const floatingCart = document.getElementById('floatingCart');
   let hasClass = floatingCart.classList.contains('open');
   if (hasClass) {
     floatingCart.classList.remove('open');
@@ -45,5 +47,35 @@ jQuery(document).ready(function ($) {
     nextArrow:
       // "<img class='a-right control-c next slick-next' src='../images/shoe_story/arrow-right.png'>",
       "<i class='SliderHomeNext icofont-curved-right a-right control-c next slick-next'></i>",
+    responsive: [
+      {
+        breakpoint: 1199.98,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 991.98,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 767.98,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 575.98,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 });
